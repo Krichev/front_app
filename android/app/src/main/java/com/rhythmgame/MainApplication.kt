71fa8e78f80e86d49da2d13def651dbab46e58a7
Alen <alen_melkonyan@mail.ru>
2026-03-22 20:52:03 +0300

    fix update react native version

--- android/app/src/main/java/com/rhythmgame/MainApplication.kt
+++ android/app/src/main/java/com/rhythmgame/MainApplication.kt
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping  // ← ADD THIS
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
@@ -36,7 +37,7 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
+        SoLoader.init(this, OpenSourceMergedSoMapping)  // ← CHANGED (was "false")
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             load()
         }
